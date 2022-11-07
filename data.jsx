25891f2ca0ecb61ff38dcdd90d6a8a2701f54a62
Sheetal Patnasetty <108509711+Sheetal-Patnasetty@users.noreply.github.com>
2022-11-07 09:31:16 +0530

    Add files via upload

---
+++ data.jsx
@@ -0,0 +1,96 @@
+import A from './components/3.jpg'
+import B from './components/11.jpg'
+import C from './components/4.jpg'
+
+import D from './components/13.png'
+import E from './components/14.png'
+import F from './components/15.jpg'
+import G from './components/16.jpg'
+
+import H from './components/6.jpg'
+import I from './components/5.jpg'
+import J from './components/7.jpg'
+import K from './components/8.jpg'
+import L from './components/9.jpg'
+import M from './components/17.jpg'
+import N from './components/18.jpg'
+import O from './components/19.jpg'
+
+export const SliderItems =[
+    {
+        id: 1,
+        image: A,
+        title: "DEEPAVALI SALE",
+        desc: "GET FLAT 40% ON EVERY PURCHASE. OFFER CLOSES SOON!",
+    },
+    {
+        id: 2,
+        image: B,
+        title: "LAKME",
+        desc: "GET FLAT 20% FOR NEW ARRIVALS.",
+    },
+    {
+        id: 3,
+        image: C,
+        title: "WINTER SALE",
+        desc: "GET FLAT 40% FOR MOISTURIZERS.",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        image: D,
+        title: "BODY",
+    },
+    {
+        id: 2,
+        image: E,
+        title: "FACE",
+    },
+    {
+        id: 3,
+        image: F,
+        title: "EYES",
+    },
+    {
+        id: 4,
+        image: G,
+        title: "LIPS",
+    },
+];
+
+export const popularproducts = [
+    {
+        id: 1,
+        image: I,
+    },
+    {
+        id: 2   ,
+        image: J,
+    },
+    {
+        id: 3,
+        image: M,
+    },
+    {
+        id: 4,
+        image: K,
+    },
+    {
+        id: 5,
+        image: H,
+    },
+    {
+        id: 6,
+        image: L,
+    },
+    {
+        id: 7,
+        image: N,
+    },
+    {
+        id: 8,
+        image: O,
+    },
+];
